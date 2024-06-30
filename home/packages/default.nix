e46ca439d11cc8f9eeddd5093b9b42b9e02f477b
ALEZ-DEV <raidersfocus@gmail.com>
2024-06-30 21:41:01 +0200

    add dev packages

--- home/packages/default.nix
+++ home/packages/default.nix
@@ -4,13 +4,10 @@
   home.packages = [
    pkgs.mpv
    pkgs.floorp
-   pkgs.neovim
    pkgs.wofi
    pkgs.qbittorrent
    pkgs.goofcord
    pkgs.localsend
-   pkgs.git
-   pkgs.gcc
    pkgs.mission-center
    pkgs.spotify
    pkgs.blueman
