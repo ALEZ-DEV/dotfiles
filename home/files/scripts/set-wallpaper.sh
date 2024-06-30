754eed0d647c79d7f2f6cf921e3873fd54c12bba
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-01 00:14:59 +0200

    fix script

--- home/files/scripts/set-wallpaper.sh
+++ home/files/scripts/set-wallpaper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 swww-daemon &
-swww img ~/Pictures/wallpaper/wallpaper.jpg
+swww img ~/Pictures/wallpaper/wallpaper.jpg --resize crop -o eDP-1 &
+swww img ~/Pictures/wallpaper/wallpaper.jpg --resize crop -o DP-2 &
+swww img ~/Pictures/wallpaper/wallpaper.jpg --resize fit -o HDMI-A-1 &
