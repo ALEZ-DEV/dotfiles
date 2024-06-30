f5c1cb320149021a8b467e897f45ca40f304ca30
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-01 00:07:55 +0200

    fix script

--- home/files/default.nix
+++ home/files/default.nix
@@ -6,7 +6,6 @@
     ".config/hypr/hyprland.conf".source = ./hyprland.conf;
 
     ".config/scripts/set-wallpaper.sh".source = ./scripts/set-wallpaper.sh;
-    ".config/scripts/start-swww-daemon.sh".source = ./scripts/start-swww-daemon.sh;
 
     "Pictures/wallpaper/wallpaper.jpg".source = ./wallpaper.jpg;
   };
