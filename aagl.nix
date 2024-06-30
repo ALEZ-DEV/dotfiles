34ea03113178abe0fa9ba5a839b16b2e3b77deb5
= <=>
2024-06-30 19:58:48 +0200

    add some game

---
+++ aagl.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+let
+  aagl = = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
+in 
+{
+  home.packages = [
+    aagl.wavey-launcher
+  ];
+}
