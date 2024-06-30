946f2c053bb593d587ce36307d596720bbb2dc42
= <=>
2024-06-30 20:40:02 +0200

    update hyprland keybinding

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@ in
   networking.networkmanager.enable = true;
   networking.extraHosts =
   ''
-    127.0.0.2 pc.crashsight.wetest.net
+    127.0.0.1 pc.crashsight.wetest.net
   ''; 
 
   hardware= {
