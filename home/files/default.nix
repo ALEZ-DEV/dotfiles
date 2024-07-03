f308bf4da3c7eb6021889bc2f72f8085d996474a
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-03 18:14:33 +0200

    reorganizing files

--- home/files/default.nix
+++ home/files/default.nix
@@ -2,15 +2,15 @@
 
 {
   imports = [
-    ./nvim.nix
+    ./nvim/default.nix
   ];
 
   home.file = {
-    ".config/alacritty/alacritty.toml".source = ./alacritty.toml;
-    ".config/hypr/hyprland.conf".source = ./hyprland.conf;
-
     ".config/scripts/set-wallpaper.sh".source = ./scripts/set-wallpaper.sh;
 
-    "Pictures/wallpaper/wallpaper.jpg".source = ./wallpaper.jpg;
+    ".config/alacritty/alacritty.toml".source = ./terminal/alacritty.toml;
+
+    ".config/hypr/hyprland.conf".source = ./wm/hyprland.conf;
+    "Pictures/wallpaper/wallpaper.jpg".source = ./wm/wallpaper.jpg;
   };
 }
