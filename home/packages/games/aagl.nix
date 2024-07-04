3cf21ca8044e3edbe28ab098bd00d0afd8299284
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-04 18:10:53 +0200

    add ZZZ game

--- home/packages/games/aagl.nix
+++ home/packages/games/aagl.nix
@@ -1,11 +1,12 @@
 { config, pkgs, lib, ... }:
 
 let
-  aagl = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/ZZZ.tar.gz");
+  aagl = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/refs/heads/zzz.zip");
 in 
 {
   home.packages = (lib.mkIf config.packages.games.enable [
     aagl.wavey-launcher
     aagl.honkers-railway-launcher
+    aagl.sleepy-launcher
   ]);
 }
