46a7e821146380aa8513a73b617718a821402584
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-18 18:52:33 +0200

    trying waybar styling

--- home/files/default.nix
+++ home/files/default.nix
@@ -16,6 +16,11 @@
     ".config/wallpaper/wallpaper.mp4".source = ./wm/wallpaper.mp4;
     ".config/wallpaper/wallpaper-littlescreen.mp4".source = ./wm/wallpaper-littlescreen.mp4;
     ".config/wallpaper/wallpaper-widescreen.mp4".source = ./wm/wallpaper-widescreen.mp4;
+
+    ".config/waybar/style.css".source = ./waybar/style.css;
+    ".config/waybar/style-bottom.css".source = ./waybar/style-bottom.css;
+    ".config/waybar/style-hyprland.css".source = ./waybar/style-hyprland.css;
+    ".config/waybar/style-options.css".source = ./waybar/style-options.css;
   };
 
   home.sessionVariables = {
--- home/packages/desktop/default.nix
+++ home/packages/desktop/default.nix
@@ -4,11 +4,87 @@
   home.packages = with pkgs; [
     wofi
     pavucontrol
-    waybar
     wl-clipboard
     grim
     slurp
     mako
     mpvpaper
+    swww
   ];
+
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+    settings = {
+      height = 36;
+      modules-left = [ "group/leftside" ];
+      modules-right = [ "hyprland/window" ];
+      layer = "top";
+
+      "hyprland/window" = {
+        format = "{} ";
+        separate-outputs = false;
+      };
+
+      "group/leftside" = {
+        orientation = "horizontal";
+        modules = [
+          "hyprland/workspaces"
+          "mpris#prefix"
+          "mpris#suffix"
+        ];
+      };
+
+      "clock#time" = {
+        format = "{:%H:%M}";
+        tooltip-format = "{:%a, %d %b %H:%M}";
+        interval = 1;
+        tooltip = false;
+      };
+
+      "clock#date" = {
+        format = "{:%a %d %b}";
+        interval = false;
+      };
+
+      "group/datetime" = {
+        orientation = "horizontal";
+        drawer = {
+          transition-left-to-right = false;
+        };
+        modules = [
+          "clock#time"
+          "clock#date"
+        ];
+      };
+
+      "battery#prefix" = {
+        format = " {icon}";
+        format-charging = " 󰂄",
+        format-icons = [
+          "󰁺"
+          "󰁼"
+          "󰁾"
+          "󰂁"
+          "󰁹"
+        ];
+        interval = 1;
+      };
+
+      "battery#suffix" = {
+        format = "{capacity}%";
+        interval = 1;
+      };
+
+      "privacy" = {
+        icon-size = 13;
+        icon-spacing = 0;
+      };
+
+      "group/network" = {
+        orientation = "horizontal";
+        modules = []; #TODO
+      }
+    };
+  };
 }
