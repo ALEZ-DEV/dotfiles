46f48ec88f3d46ee3759eba182acdd0e8eeda72c
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-25 22:11:52 +0200

    add gimp package

--- home/packages/office/default.nix
+++ home/packages/office/default.nix
@@ -4,6 +4,7 @@
   home.packages = lib.mkIf config.packages.office.enable (with pkgs; [
     goofcord
     spotify
+    gimp
     #obsidian # don't work because of nvidia, so I use the flatpak one for the time being, https://discourse.nixos.org/t/drm-kernel-driver-nvidia-drm-in-use-nvk-requires-nouveau/42222/16
   ]);
 }
