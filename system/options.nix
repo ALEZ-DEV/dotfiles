94eb141624f2b91a63255773111fe93752df8dc8
ALEZ-DEV <raidersfocus@gmail.com>
2024-08-29 19:20:12 +0200

    add hostname option

--- system/options.nix
+++ system/options.nix
@@ -37,11 +37,11 @@
         default = builtins.getEnv "HOME";
         description = "Default home folder of the current user";
       };
-      pc-name = mkOption {
-        type = types.string;
-        default = builtins.getEnv "HOST";
-        description = "Default PC name of the current computer";
-      };
+    };
+    hostname = mkOption {
+      type = types.string;
+      default = builtins.getEnv "HOST";
+      description = "Default PC name of the current computer";
     };
   };
 }
