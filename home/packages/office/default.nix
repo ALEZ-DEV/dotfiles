970333c462cb344bd55c8d058f9fde85461665fd
ALEZ-DEV <raidersfocus@gmail.com>
2024-08-29 19:17:15 +0200

    add appflowy

--- home/packages/office/default.nix
+++ home/packages/office/default.nix
@@ -6,5 +6,6 @@
     spotify
     gimp
     #obsidian # don't work because of nvidia, so I use the flatpak one for the time being, https://discourse.nixos.org/t/drm-kernel-driver-nvidia-drm-in-use-nvk-requires-nouveau/42222/16
+    appflowy
   ]);
 }
