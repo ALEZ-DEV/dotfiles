71c4ca0d2c98cb1165f1bc98bc44e678e86be07c
ALEZ-DEV <raidersfocus@gmail.com>
2024-09-17 23:10:18 +0200

    add some package for the office

--- home/packages/office/default.nix
+++ home/packages/office/default.nix
@@ -7,5 +7,7 @@
     gimp
     #obsidian # don't work because of nvidia, so I use the flatpak one for the time being, https://discourse.nixos.org/t/drm-kernel-driver-nvidia-drm-in-use-nvk-requires-nouveau/42222/16
     appflowy
+    qalculate-gtk
+    #cura using the flatpak for now because some python packages to build this derivation, https://github.com/NixOS/nixpkgs/issues/325896
   ]);
 }
