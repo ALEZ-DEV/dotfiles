8da6c079c9b8b016405f7a2e89fb1280f22b610f
ALEZ-DEV <raidersfocus@gmail.com>
2024-10-08 21:00:48 +0200

    add some office packages

--- home/packages/office/default.nix
+++ home/packages/office/default.nix
@@ -4,7 +4,9 @@
   home.packages = lib.mkIf config.packages.office.enable (with pkgs; [
     goofcord
     spotify
+    spotdl
     gimp
+    blender
     #obsidian # don't work because of nvidia, so I use the flatpak one for the time being, https://discourse.nixos.org/t/drm-kernel-driver-nvidia-drm-in-use-nvk-requires-nouveau/42222/16
     appflowy
     qalculate-gtk
