b932662e7e9c997fafbd2ce927e66a50b4b5d8c3
ALEZ-DEV <raidersfocus@gmail.com>
2024-10-11 19:35:14 +0200

    update office packages

--- home/packages/office/default.nix
+++ home/packages/office/default.nix
@@ -6,10 +6,8 @@
     spotify
     spotdl
     gimp
-    blender
-    #obsidian # don't work because of nvidia, so I use the flatpak one for the time being, https://discourse.nixos.org/t/drm-kernel-driver-nvidia-drm-in-use-nvk-requires-nouveau/42222/16
+    obsidian # don't work because of nvidia, so I use the flatpak one for the time being, https://discourse.nixos.org/t/drm-kernel-driver-nvidia-drm-in-use-nvk-requires-nouveau/42222/16
     appflowy
-    qalculate-gtk
-    #cura using the flatpak for now because some python packages to build this derivation, https://github.com/NixOS/nixpkgs/issues/325896
+    teams-for-linux
   ]);
 }
