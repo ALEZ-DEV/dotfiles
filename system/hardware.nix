1bb34070e5c6f77457ca422bc2646a90c9a5f838
ALEZ-DEV <raidersfocus@gmail.com>
2024-11-15 20:37:21 +0100

    add some qmk related package

--- system/hardware.nix
+++ system/hardware.nix
@@ -8,9 +8,14 @@
     };
 
     steam-hardware.enable = true;
+
+    keyboard.qmk.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
     opentabletdriver
+    via
+    avrdude
+    pico-sdk
   ];
 }
