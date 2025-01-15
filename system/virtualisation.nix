e9994d08530664ca6dabdea64aa068f8d824b031
ALEZ-DEV <raidersfocus@gmail.com>
2025-01-15 15:29:57 +0100

    add user in docker group

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,15 +1,21 @@
 { config, pkgs, ... }:
 
 {
+  programs.virt-manager.enable = true;
+
   virtualisation = {
     containers.enable = true;
     waydroid.enable = true;
     lxd.enable = true;
     spiceUSBRedirection.enable = true;
     docker.enable = true;
+    libvirtd.enable = true;
   };
 
-  users.users.alez.extraGroups = [ "docker" ];
+  users = {
+    users.alez.extraGroups = [ "docker" ];
+    groups.libvirtd.members = ["alez"];
+  };
 
   environment.systemPackages = with pkgs; [
     quickemu
