f0eabc890a36d6c0b9c3767e48b3537ecae32684
ALEZ-DEV <me@al3z.dev>
2025-04-17 22:49:49 +0200

    refactor gpu naming

--- hosts/home-desktop/hardware-configuration.nix
+++ hosts/home-desktop/hardware-configuration.nix
@@ -45,8 +45,9 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.opengl = {
-    driSupport32Bit = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
 
     extraPackages = with pkgs; [
       rocmPackages.clr.icd
