63821ad93a5acc222930a568833f6739c7dabfde
ALEZ-DEV <me@al3z.dev>
2026-05-24 19:44:07 +0200

    add some file

---
+++ home/files/pipewire/gameshare-to-headphones.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pw-link "Game-Share:monitor_FL" "alsa_output.usb-Logitech_PRO_000000000000-00.analog-stereo:playback_FL"
+pw-link "Game-Share:monitor_FR" "alsa_output.usb-Logitech_PRO_000000000000-00.analog-stereo:playback_FR"
